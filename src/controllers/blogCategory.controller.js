7754b160fba35f3c1e829fd0741ad4df9e4138b9
fatihbaydar <fatihbaydar2004@gmail.com>
2024-12-08 09:50:15 +0300

    blogCategoryController

--- src/controllers/blogCategory.controller.js
+++ src/controllers/blogCategory.controller.js
@@ -1,4 +1,59 @@
 "use strict";
 
+const { BlogCategory } = require("../models/blogCategory.model")
 
+// BlogCategory Controller:
+// /blog/category
 
+module.exports.blogCategory = {
+    list: async (req, res) => {
+        const data = await BlogCategory.find()
+        /* const data = await BlogCategory.find({}) */ //yukardaki ile aynı
+        res.send({
+            result: data
+        })
+    },
+
+    //? CRUD
+    create: async (req, res) => {
+        const result = await BlogCategory.create(req.body)
+        res.send({ // kısayol olarak doğrudan result'ı gönder
+            result
+        })
+    },
+
+    read: async (req, res) => {
+        const result = await BlogCategory.findOne({ _id: req.params.categoryId }, { _id: 0, name: 1 }) //ilk alan filtreleme. Veride istenmeyen field varsa false yada 0 ile döndürülmeyebilir
+        res.send({
+            isError: false,
+            result
+        })
+    },
+
+    update: async (req, res) => {
+        const result = await BlogCategory.updateOne({ _id: req.params.categoryId }, req.body)
+        //!güncellenmek istenen veri yoksa:
+        if (!result.matchedCount === 0) {
+            // throw new CustomError("Eşleşen veri bulunamadı")
+            return res.status(404).send("Eşleşen veri bulunamadı")
+        }
+        //! güncellenmek istenen veri var ama zaten güncel olduğu için güncelleme yapılmadı:
+        if (result.matchedCount > 0 && result.modifiedCount === 0) {
+            return res.status(200).send({ message: "Belge zaten güncel." });
+        }
+        res.status(202).send({
+            isError: false,
+            result,
+            updated: await BlogCategory.findOne({ _id: req.params.categoryId }),
+        });
+    },
+
+    delete: async (req, res) => {
+        const result = await this.BlogCategory.deleteOne({ _id: req.params.categoryId })
+        res.send({
+            result
+        })
+    },
+};
+
+/* ------------------------------------------------------- */
