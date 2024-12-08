85ed14beb82e37e27c8ca8d2d9ac9a683d47db33
fatihbaydar <fatihbaydar2004@gmail.com>
2024-12-08 17:43:22 +0300

    routers

---
+++ src/routes/blogCategory.router.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const router = require("express").Router();
+const { blogCategory } = require("../controllers/blogCategory.controller")
+
+// Call Controllers:
+// /blog/category
+// /blog/category/id
+
+
+/* ------------------------------------------------------- */
+
+// URL: /blog ->
+// BlogCategory
+router.route("/category").get(blogCategory.list).post(blogCategory.create)
+// router.get("/category", blogCategory.list)
+// router.post("/category", blogCategory.create)
+router.route("/category/:categoryId")
+    .get(blogCategory.read)
+    .put(blogCategory.update)
+    .patch(blogCategory.update)
+    .delete(blogCategory.delete)
+
+module.exports = router
--- src/routes/blogPost.router.js
+++ src/routes/blogPost.router.js
@@ -2,13 +2,20 @@
 
 
 const router = require("express").Router();
-
+const {blogPost} = require("../controllers/blogPost.controller")
 // Call Controllers:
 
 /* ------------------------------------------------------- */
 
 // URL: /blog ->
-
+// /blog/post
 // BlogPost
+router.route("/post").get(blogPost.list).post(blogPost.create)
+router.route("/post/:postId")
+    .get(blogPost.read)
+    .put(blogPost.update)
+    .patch(blogPost.update)
+    .delete(blogPost.delete)
 
-/* ------------------------------------------------------- */
+module.exports = router
+
