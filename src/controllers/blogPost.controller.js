89703a67d182e8a2dbec13e2660428e451801bbc
fatihbaydar <fatihbaydar2004@gmail.com>
2024-12-19 17:08:28 +0300

    filter,search,sort,pagination

--- src/controllers/blogPost.controller.js
+++ src/controllers/blogPost.controller.js
@@ -6,7 +6,47 @@ const { NotFoundError } = require("../errors/customError");
 
 module.exports.blogPost = {
     list: async (req, res) => {
-        const data = await BlogPost.find()
+
+        //! FİLTRELEME - ARAMA - SIRALAMA - SAYFALAMA 
+
+        // console.log(req.query)
+
+        //! FİLTRELEME
+        // URL?filter[fieldName]
+        const filter = req.query?.filter || {}
+        // console.log(filter)
+
+        //! ARAMA
+        // URL?search[fieldName]
+        const search = req.query?.search || {}
+        // console.log(search)
+        for (let key in search) {
+            search[key] = { $regex: search[key], $options: "i" }
+        }
+        // console.log(search)
+
+        //! SIRALAMA
+        // URL?sort[fieldName1]=asc&sort[fieldName2]=desc (asc: A-Z, desc: Z-A)
+        const sort = req.query?.sort || {}
+
+        //! SAYFALAMA
+
+        //* LIMIT
+        let limit = Number(req.query.limit)
+        limit = limit > 0 ? limit :Number(process.env?.PAGE_SIZE ||20)
+        // console.log(limit, typeof limit)
+
+        //*SAYFA
+        let page = Number(req.query.page)
+        page = page > 0 ? page:1
+
+        //*SKIP
+        let skip = Number(req.query.skip)
+        skip = skip > 0 ? skip:((page-1)*limit)
+        console.log(page,skip,limit)
+
+        // const data = await BlogPost.find().populate("categoryId")
+        const data = await BlogPost.find({ ...filter, ...search }).sort(sort).limit(limit).skip(skip).populate(["userId", "categoryId"])
 
         res.send({
             result: data,
@@ -17,7 +57,7 @@ module.exports.blogPost = {
 
     create: async (req, res) => {
         //login olunmuşsa userId'yi req.user'dan alma
-        if(req.user) req.body.userId = req.user._id
+        if (req.user) req.body.userId = req.user._id
 
         const result = await BlogPost.create(req.body);
 
@@ -45,12 +85,12 @@ module.exports.blogPost = {
 
         //!güncellenmek istenen veri yoksa
         if (result.matchedCount === 0) {
-          throw new NotFoundError("Eşleşen veri bulunamadı");
-        // return res.status(404).send("Eşleşen veri bulunamadı");
+            throw new NotFoundError("Eşleşen veri bulunamadı");
+            // return res.status(404).send("Eşleşen veri bulunamadı");
         }
         //! güncellenmek istenen veri var ama zaten güncel olduğu için güncelleme yapılmadı:
         if (result.matchedCount > 0 && result.modifiedCount === 0) {
-          return res.status(200).send({ message: "Belge zaten güncel." });
+            return res.status(200).send({ message: "Belge zaten güncel." });
         }
         res.status(202).send({
             isError: false,
