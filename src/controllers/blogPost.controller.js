00c827b2e5cc12a0ce4407041372f847b9b2385c
fatihbaydar <fatihbaydar2004@gmail.com>
2024-12-19 17:22:30 +0300

    created new middleware

--- src/controllers/blogPost.controller.js
+++ src/controllers/blogPost.controller.js
@@ -7,46 +7,48 @@ const { NotFoundError } = require("../errors/customError");
 module.exports.blogPost = {
     list: async (req, res) => {
 
-        //! FİLTRELEME - ARAMA - SIRALAMA - SAYFALAMA 
+        // //! FİLTRELEME - ARAMA - SIRALAMA - SAYFALAMA 
 
-        // console.log(req.query)
+        // // console.log(req.query)
 
-        //! FİLTRELEME
-        // URL?filter[fieldName]
-        const filter = req.query?.filter || {}
-        // console.log(filter)
+        // //! FİLTRELEME
+        // // URL?filter[fieldName]
+        // const filter = req.query?.filter || {}
+        // // console.log(filter)
 
-        //! ARAMA
-        // URL?search[fieldName]
-        const search = req.query?.search || {}
-        // console.log(search)
-        for (let key in search) {
-            search[key] = { $regex: search[key], $options: "i" }
-        }
-        // console.log(search)
+        // //! ARAMA
+        // // URL?search[fieldName]
+        // const search = req.query?.search || {}
+        // // console.log(search)
+        // for (let key in search) {
+        //     search[key] = { $regex: search[key], $options: "i" }
+        // }
+        // // console.log(search)
 
-        //! SIRALAMA
-        // URL?sort[fieldName1]=asc&sort[fieldName2]=desc (asc: A-Z, desc: Z-A)
-        const sort = req.query?.sort || {}
+        // //! SIRALAMA
+        // // URL?sort[fieldName1]=asc&sort[fieldName2]=desc (asc: A-Z, desc: Z-A)
+        // const sort = req.query?.sort || {}
 
-        //! SAYFALAMA
+        // //! SAYFALAMA
 
-        //* LIMIT
-        let limit = Number(req.query.limit)
-        limit = limit > 0 ? limit :Number(process.env?.PAGE_SIZE ||20)
-        // console.log(limit, typeof limit)
+        // //* LIMIT
+        // let limit = Number(req.query.limit)
+        // limit = limit > 0 ? limit :Number(process.env?.PAGE_SIZE ||20)
+        // // console.log(limit, typeof limit)
 
-        //*SAYFA
-        let page = Number(req.query.page)
-        page = page > 0 ? page:1
+        // //*SAYFA
+        // let page = Number(req.query.page)
+        // page = page > 0 ? page:1
 
-        //*SKIP
-        let skip = Number(req.query.skip)
-        skip = skip > 0 ? skip:((page-1)*limit)
-        console.log(page,skip,limit)
+        // //*SKIP
+        // let skip = Number(req.query.skip)
+        // skip = skip > 0 ? skip:((page-1)*limit)
+        // console.log(page,skip,limit)
 
-        // const data = await BlogPost.find().populate("categoryId")
-        const data = await BlogPost.find({ ...filter, ...search }).sort(sort).limit(limit).skip(skip).populate(["userId", "categoryId"])
+        // // const data = await BlogPost.find().populate("categoryId")
+        // const data = await BlogPost.find({ ...filter, ...search }).sort(sort).limit(limit).skip(skip).populate(["userId", "categoryId"])
+
+        // Fİltreleme ... başlığı ile başlayan kısımdan buraya kadar olan yer findSearchSortPage.js'e taşındı.
 
         res.send({
             result: data,
---
+++ src/middlewares/findSearchSortPage.js
@@ -0,0 +1,46 @@
+"use strict";
+
+module.exports = async (req, res, next) => {
+    //! FİLTRELEME - ARAMA - SIRALAMA - SAYFALAMA 
+
+    // console.log(req.query)
+
+    //! FİLTRELEME
+    // URL?filter[fieldName]
+    const filter = req.query?.filter || {}
+    // console.log(filter)
+
+    //! ARAMA
+    // URL?search[fieldName]
+    const search = req.query?.search || {}
+    // console.log(search)
+    for (let key in search) {
+        search[key] = { $regex: search[key], $options: "i" }
+    }
+    // console.log(search)
+
+    //! SIRALAMA
+    // URL?sort[fieldName1]=asc&sort[fieldName2]=desc (asc: A-Z, desc: Z-A)
+    const sort = req.query?.sort || {}
+
+    //! SAYFALAMA
+
+    //* LIMIT
+    let limit = Number(req.query.limit)
+    limit = limit > 0 ? limit : Number(process.env?.PAGE_SIZE || 20)
+    // console.log(limit, typeof limit)
+
+    //*SAYFA
+    let page = Number(req.query.page)
+    page = page > 0 ? page : 1
+
+    //*SKIP
+    let skip = Number(req.query.skip)
+    skip = skip > 0 ? skip : ((page - 1) * limit)
+    console.log(page, skip, limit)
+
+    // const data = await BlogPost.find().populate("categoryId")
+    const data = await BlogPost.find({ ...filter, ...search }).sort(sort).limit(limit).skip(skip).populate(["userId", "categoryId"])
+    next()
+}
+
